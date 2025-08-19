9741c9e95807e565ec133c63061c4edb79da0bd5
Adeel Ahmad <6880680+adeelahmadk@users.noreply.github.com>
2025-08-19 15:59:28 +0500

    fix(dev/mysql): fix setup script bug
    Fixed root priviledges issue for setup script.

--- dev/mysql/mysql-setup-db-user.sh
+++ dev/mysql/mysql-setup-db-user.sh
@@ -5,30 +5,28 @@
 #########################################################################################
 
 function usage() {
-	echo "usage: `basename $0` -d db -u db_user -p user_pass -a root_user -r root_pass"
+  echo "usage: `basename $0` -d db -u db_user -p user_pass -a root_user"
 }
 
-ROOTPASS=
-
-while getopts :d:u:p:a:r: option
+while getopts :d:u:p:a: option
 do
-	case "${option}"
-	in
-	d) DBNAME=${OPTARG};;
-	u) DBUSER=${OPTARG};;
-	p) DBPASS=${OPTARG};;
-	a) ROOTUSER=${OPTARG};;
-	r) ROOTPASS=${OPTARG};;
-	\?) echo "Invalid Option: -$OPTARG";;
-	:)  echo "Option -$OPTARG requires an argument.";;
-	esac
+  case "${option}"
+  in
+  d) DBNAME=${OPTARG};;
+  u) DBUSER=${OPTARG};;
+  p) DBPASS=${OPTARG};;
+  a) ROOTUSER=${OPTARG};;
+  \?) echo "Invalid Option: -$OPTARG";;
+  :)  echo "Option -$OPTARG requires an argument.";;
+  esac
 done
 
 [ -z "$ROOTUSER" ] && { echo "root user name required to execute operations" >&2; usage; exit 1; }
 
 [ -z "$DBNAME" -o -z "$DBUSER" -o -z "$DBPASS" ] && { usage; exit 1; }
 
-mysql -u$ROOTUSER -p$ROOTPASS -e "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-mysql -u$ROOTUSER -p$ROOTPASS -e "CREATE USER '$DBUSER'@'localhost' IDENTIFIED BY '$DBPASS';"
-mysql -u$ROOTUSER -p$ROOTPASS -e "GRANT ALL PRIVILEGES ON \`$DBNAME\`.* TO '$DBUSER'@'localhost';"
-mysql -u$ROOTUSER -p$ROOTPASS -e "FLUSH PRIVILEGES;"
+sudo -v
+sudo mysql -u$ROOTUSER -e "CREATE DATABASE IF NOT EXISTS \`$DBNAME\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+sudo mysql -u$ROOTUSER -e "CREATE USER '$DBUSER'@'localhost' IDENTIFIED BY '$DBPASS';"
+sudo mysql -u$ROOTUSER -e "GRANT ALL PRIVILEGES ON \`$DBNAME\`.* TO '$DBUSER'@'localhost';"
+sudo mysql -u$ROOTUSER -e "FLUSH PRIVILEGES;"
